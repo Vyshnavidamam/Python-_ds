d7f1db4e348b128685aacb509bbcd23b2bb8126f
Vyshnavidamam <vyshudamam7@gmail.com>
2026-08-11 18:59:47 +0530

    Add files via upload

---
+++ formal and actual.py
@@ -0,0 +1,19 @@
+import time
+def Test_case1():
+	print("welcome to oython as functional programming language")
+if(__name__=="__main__"):
+		Test_case1()
+		print()
+		Test_case1()
+		print()
+		Test_case1()
+		print()
+time.sleep(2)
+print("end of the application")
+
+
+def Test_case1(eid,enname):
+   print("eid is :",eid)
+   print("enname")
+Test_case1(2401717007,"vyshu")
+      
